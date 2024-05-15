0ac1e90f7626017885fa0309e50f36a3a6c92604
Mariana0102 <158349418+Mariana0102@users.noreply.github.com>
2024-05-14 22:17:26 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'wagzedrak@gmail.com'
+EMAIL_HOST_USER = 'marimorales2903@gmail.com'
 EMAIL_HOST_PASSWORD = 'skworooojasuaknv'
 EMAIL_USE_TLS = True
